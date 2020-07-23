6a9430411358fdd01975464f8edc20f87352bd15
Carvis7 <carlydavis91@gmail.com>
2020-07-24 09:02:18 +1000

    new

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import React from 'react';
-import logo from './synchit-logo.png';
-import Calendar from 'react-calendar';
-import styles from './Calendar.scss';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Signin from "./pages/Signin";
 
+const font = "'Montserrat', sans-serif"; 
 
-const App = () => {
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: font,
+    button: {
+      textTransform: 'none'
+    },
+  },
+  palette: {
+    primary: {
+      main: '#18A0FB',
+    },
+  },
+});
+
+function App() {
   return (
-    <div>
-      <h1>
-        SynchIT
-      </h1>
-      <p className={styles.calendar}>
-      <Calendar />
-      <logo />
-      </p>
-    </div>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <div style={{fontFamily: font}}>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <div style={{height: 'calc(100vh - 120px)', overflow: 'auto'}}>
+            <Switch>
+              <Route path="/signup">
+                <Signup />
+              </Route>
+              <Route path="/signin">
+                <Signin />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </Router>
   );
 }
 
--- src/components/TeamCalendar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Calendar from 'react-calendar';
-
-
-
-
-
-export default TeamCalendar
